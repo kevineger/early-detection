869c7fa39d683f0d4ad32296d705660704b5da36
Kevin <kevin.eger@hotmail.com>
2015-09-10 12:19:10 -0700

    Removes unique requirement on project name

--- app/Http/Requests/ProjectRequest.php
+++ app/Http/Requests/ProjectRequest.php
@@ -24,7 +24,7 @@ class ProjectRequest extends Request
     public function rules()
     {
         return [
-            'name' => 'required | unique:projects',
+            'name' => 'required',
             'description' => 'required'
         ];
     }
--- database/migrations/2015_08_17_210231_create_projects_table.php
+++ database/migrations/2015_08_17_210231_create_projects_table.php
@@ -14,7 +14,7 @@ class CreateProjectsTable extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name')->unique();
+            $table->string('name');
             $table->text('description');
             $table->integer('project_category_id')->unsigned();
             $table->foreign('project_category_id')
