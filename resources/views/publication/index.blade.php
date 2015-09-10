00db517c4f4a28e26a09c41476610bbd6254df51
Kevin <kevin.eger@hotmail.com>
2015-09-10 14:47:30 -0700

    Modifies view

--- resources/views/publication/index.blade.php
+++ resources/views/publication/index.blade.php
@@ -7,10 +7,9 @@
                 <h2>{{ $key }}</h2>
                 @foreach( $year as $publication )
                     @if( empty($publication->url) )
-                        <h4>{{ $publication->name }}</h4>
-                        <h5 style="margin-left:10px;">{{ $publication->reference }}</h5>
+                        <h4 style="margin-left:10px;">{{ $publication->reference }}</h4>
                     @else
-                        <a href="{{ $publication->url }}"><h4>{{ $publication->name }}</h4></a>
+                        <a href="{{ $publication->url }}"><h4 style="margin-left:10px;">{{ $publication->reference }}</h4></a>
                     @endif
                 @endforeach
             </div>
