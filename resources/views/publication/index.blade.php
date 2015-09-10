772cd11df969a7d995e1a5f6a24f3ab772c3dbc0
Kevin <kevin.eger@hotmail.com>
2015-09-10 14:40:05 -0700

    Shows reference on publications

--- resources/views/publication/index.blade.php
+++ resources/views/publication/index.blade.php
@@ -8,6 +8,7 @@
                 @foreach( $year as $publication )
                     @if( empty($publication->url) )
                         <h4>{{ $publication->name }}</h4>
+                        <h5 style="margin-left:10px;">{{ $publication->reference }}</h5>
                     @else
                         <a href="{{ $publication->url }}"><h4>{{ $publication->name }}</h4></a>
                     @endif
